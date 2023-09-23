585aa2a07766b71010cbe40ead491d70d974307b
Aayush kumar <aayushkumarhigh@gmail.com>
2023-09-23 20:32:46 +0530

    fixed

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 import express from "express";
-import { config } from "dotenv";
-import ErrorMiddleware from "./middlewares/Error.js";
+import dotenv from "dotenv";
+import ErrorMiddleWare from "./middleware/Error.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-config({
-  path: "./config/config.env",
-});
+dotenv.config({ path: "./config/config.env" });
 const app = express();
 
-// Using Middlewares
+// Using Middleware
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -24,13 +22,11 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-// Importing & Using Routes
+/*Importing & using Routes */
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
 import payment from "./routes/paymentRoutes.js";
 import other from "./routes/otherRoutes.js";
-
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
@@ -38,10 +34,10 @@ app.use("/api/v1", other);
 
 export default app;
 
-app.get("/", (req, res) =>
+app.get("/", (req, res) => {
   res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
-);
+    `<h1>Site is Working. Click <a href=${process.env.FRONTEND_URL}>Here</a> to visit frontend.</h1>`
+  );
+});
 
-app.use(ErrorMiddleware);
+app.use(ErrorMiddleWare);
