24352b968ccdeee1b44625bd0be3b37a02735ac1
Aayush kumar <aayushkumarhigh@gmail.com>
2023-09-23 20:53:11 +0530

    jiji

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     "access-control-allow-credentials":true
